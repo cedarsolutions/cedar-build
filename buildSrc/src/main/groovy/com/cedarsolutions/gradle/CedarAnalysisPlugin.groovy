44f537dd371879564d0232c67556e430a5ed60f2
Kenneth J. Pronovici <pronovic@ieee.org>
2014-09-26 15:09:57 -0500

    Add support for "analysis" like code metrics and coverage.

---
+++ buildSrc/src/main/groovy/com/cedarsolutions/gradle/CedarAnalysisPlugin.groovy
@@ -0,0 +1,95 @@
+// vim: set ft=groovy ts=4 sw=4:
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// *
+// *              C E D A R
+// *          S O L U T I O N S       "Software done right."
+// *           S O F T W A R E
+// *
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// *
+// * Copyright (c) 2014 Kenneth J. Pronovici.
+// * All rights reserved.
+// *
+// * This program is free software; you can redistribute it and/or
+// * modify it under the terms of the Apache License, Version 2.0.
+// * See LICENSE for more information about the licensing terms.
+// *
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// *
+// * Author   : Kenneth J. Pronovici <pronovic@ieee.org>
+// * Language : Gradle (>= 1.7)
+// * Project  : Common Gradle Build Functionality
+// *
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+package com.cedarsolutions.gradle
+
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+
+import org.gradle.testing.jacoco.plugins.JacocoPlugin;
+import org.gradle.api.plugins.quality.CheckstylePlugin;
+
+import org.gradle.util.GUtil;
+import org.gradle.api.internal.plugins.StartScriptGenerator;
+
+/**
+ * The cedarAnalysis plugin.
+ * @author Kenneth J. Pronovici <pronovici@ieee.org>
+ */
+class CedarAnalysisPlugin implements Plugin<Project> {
+
+    /** Apply the plugin. */
+    void apply(Project project) {
+        project.plugins.apply(JacocoPlugin)
+        project.plugins.apply(CheckstylePlugin)
+
+        project.extensions.create("cedarAnalysis", CedarAnalysisPluginExtension, project)
+        project.convention.plugins.cedarAnalysis = new CedarAnalysisPluginConvention(project)
+
+        applyCedarAnalysis(project)
+    }
+
+    /** Apply cedarAnalysis. */
+    void applyCedarAnalysis(Project project) {
+
+        // Configure the standard Checkstyle plugin
+        project.checkstyle {
+            configFile = project.file("doc/standards/CheckstyleRules.xml")
+            reportsDir = project.file("build/reports/checkstyle")
+            ignoreFailures = true
+            showViolations = false
+            toolVersion = 5.5
+            configProperties = [ config_loc : project.file(project.projectDir.canonicalPath + "/doc/standards").canonicalPath, ]
+        }
+
+        // Configure the standard Jacoco coverage report
+        project.jacocoTestReport {
+            reports {
+                xml.enabled false
+                csv.enabled false
+                html.enabled false
+            }
+        }
+
+        // Generate JavaNCSS metrics for the configured source directories
+        project.task("javancss") << {
+            project.convention.plugins.cedarAnalysis.runJavancss("Source Code", project.cedarAnalysis.getSourceFolders())
+            project.convention.plugins.cedarAnalysis.runJavancss("Test Code", project.cedarAnalysis.getTestFolders())
+        }
+
+        // Run all of the configured metrics.
+        project.task("metrics", dependsOn: [ project.tasks.javancss, ]) << {
+        }
+
+        // Create a single checkstyle task
+        project.task("checkstyle", dependsOn: [ project.tasks.checkstyleMain, project.tasks.checkstyleTest, ]) << {
+        }
+
+        // Create a single coverage task
+        project.task("coverage", dependsOn: [ project.tasks.jacocoTestReport, ]) << {
+        }
+
+    }
+
+}
---
+++ buildSrc/src/main/groovy/com/cedarsolutions/gradle/CedarAnalysisPluginConvention.groovy
@@ -0,0 +1,84 @@
+// vim: set ft=groovy ts=4 sw=4:
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// *
+// *              C E D A R
+// *          S O L U T I O N S       "Software done right."
+// *           S O F T W A R E
+// *
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// *
+// * Copyright (c) 2014 Kenneth J. Pronovici.
+// * All rights reserved.
+// *
+// * This program is free software; you can redistribute it and/or
+// * modify it under the terms of the Apache License, Version 2.0.
+// * See LICENSE for more information about the licensing terms.
+// *
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+// *
+// * Author   : Kenneth J. Pronovici <pronovic@ieee.org>
+// * Language : Gradle (>= 1.7)
+// * Project  : Common Gradle Build Functionality
+// *
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+package com.cedarsolutions.gradle
+
+import org.gradle.api.Project
+import groovy.swing.SwingBuilder
+import javax.swing.JFrame
+import java.util.Properties
+
+/**
+ * Plugin convention for cedarAnalysis.
+ * @author Kenneth J. Pronovici <pronovic@ieee.org>
+ */
+class CedarAnalysisPluginConvention {
+
+   /** Project tied to this convention. */
+   private Project project;
+
+   /** Create a convention for a project. */
+   public CedarAnalysisPluginConvention(Project project) {
+        this.project = project;
+   }
+
+   /**
+    * Run JavaNCSS for a set of source folders on a single command-line.
+    * This generates output onto the console as well as a complete XML report.
+    * @param sourceFolders  List of source folders
+    * See: http://stackoverflow.com/questions/7111362
+    * See: http://stackoverflow.com/questions/13474842
+    * See: http://stackoverflow.com/questions/19687050
+    */
+   def runJavancss(name, folders) {
+
+        def reportDir = project.file(project.projectDir.canonicalPath + "/build/reports/ncss").canonicalPath
+        def reportFile = project.file(reportDir + "/report.xml").canonicalPath
+
+        project.file(reportDir).mkdirs()
+        project.javaexec {
+            classpath project.configurations.analysis
+            main = "javancss.Main"
+            args = [ "-recursive", "-all", "-xml", "-out", reportFile ] + folders
+        }
+
+        println ""
+        println "================================================================="
+        println "JavaNCSS Analysis for " + name
+        println "================================================================="
+        println "See: http://www.kclee.de/clemens/java/javancss/"
+        println ""
+
+        project.javaexec {
+            classpath project.configurations.analysis
+            main = "javancss.Main"
+            args = [ "-recursive", "-ncss", "-package", ] + folders
+        }
+
+        println ""
+
+
+   }
+
+}
